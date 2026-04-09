9abcf88ad16f603770510749e39fe4b9d4f26e1d
THETOXICSIDEOFME <rayen.stark@protonmail.com>
2026-04-09 14:16:21 +0100

    fixed routing ports for job service and subscription service and added better json sterelization

--- job-service/src/main/java/com/monicahire/job_service/JobServiceApplication.java
+++ job-service/src/main/java/com/monicahire/job_service/JobServiceApplication.java
@@ -2,6 +2,16 @@ package com.monicahire.job_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @SpringBootApplication
 public class JobServiceApplication {
@@ -9,5 +19,17 @@ public class JobServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JobServiceApplication.class, args);
 	}
+	@Bean
+public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.registerModule(new JavaTimeModule());
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+    JsonSerializer<Object> serializer = new JsonSerializer<>(mapper);
+    DefaultKafkaProducerFactory<String, Object> factory =
+            new DefaultKafkaProducerFactory<>(producerFactory.getConfigurationProperties(), new StringSerializer(), serializer);
+
+    return new KafkaTemplate<>(factory);
+}
 
 }
